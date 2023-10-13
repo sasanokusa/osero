b18e0b79eec2ead473c9ccb68efa72db029abcb5
nyahahanoha <rintarooono@gmail.com>
2023-10-13 09:11:33 +0900

    complete can_set

--- board.py
+++ board.py
@@ -1,4 +1,4 @@
-class board:
+class Board:
     def __init__(self):
         self.board = [[0, 0, 0, 0, 0, 0, 0, 0],
                       [0, 0, 0, 0, 0, 0, 0, 0],
@@ -10,16 +10,45 @@ class board:
                       [0, 0, 0, 0, 0, 0, 0, 0]]
 
     def search_color(self, pos, color):
-        x, y = pos
+        y, x = pos
         other_color = color * -1
         l = []
         for i in range(-1, 2):
             for j in range(-1, 2):
-                if (not 8 >= x+i >= 0 and 8 >= y+j >= 0) \
+                if (not 7 >= x+j >= 0 and 7 >= y+i >= 0) \
                                     or (i == 0 and j == 0):
                     continue
-                set_pos = (x+i, y+j)
-
-                if self.board[set_pos[0]][set_pos[1]] == other_color:
+                if self.board[y+i][x+j] == other_color:
                     l.append((i, j))
         return l
+
+    def can_set(self, pos, color):
+        y0, x0 = pos
+        search_color_list = self.search_color(pos, color)
+        if search_color_list == []:
+            return False
+        for i in range(len(search_color_list)):
+            direction = search_color_list[i]
+            for n in range(2, 8, 1):
+                y = y0+(n*direction[0])
+                x = x0+(n*direction[1])
+                if not 0 <= x <= 7 or not 0 <= y <= 7:
+                    break
+                if self.board[y][x] == color:
+                    return True
+        return False
+
+if __name__ == "__main__":
+    pass
+    board = Board()
+    board.board = [[0, 0, 0, 0, 0, 0, 0, 0], #0
+                   [0, 0, 0, 0, 0, 0, 0, 0], #1
+                   [0, 0, 0, 0, 0, 0, 0, 0], #2
+                   [0, 0,-1,-1,-1, 0, 0, 0], #3
+                   [0, 0, 1, 1, 1, 0, 0, 0], #4
+                   [0, 0, 0, 0, 0, 0, 0, 0], #5
+                   [0, 0, 0, 0, 0, 0, 0, 0], #6
+                   [0, 0, 0, 0, 0, 0, 0, 0]] #7
+    #assert set(board.search_color((5, 2), -1)) == {(-1, 0), (-1, 1)}
+    #print(board.search_color((5, 3), -1))
+    assert board.can_set((5, 2), -1) == True
---
+++ test_board.py
@@ -0,0 +1,66 @@
+import pytest
+from board import Board
+
+def test_Board():
+    board = Board()
+    assert board.board == [[0, 0, 0, 0, 0, 0, 0, 0], #0
+                           [0, 0, 0, 0, 0, 0, 0, 0], #1
+                           [0, 0, 0, 0, 0, 0, 0, 0], #2
+                           [0, 0, 0, 1,-1, 0, 0, 0], #3
+                           [0, 0, 0,-1, 1, 0, 0, 0], #4
+                           [0, 0, 0, 0, 0, 0, 0, 0], #5
+                           [0, 0, 0, 0, 0, 0, 0, 0], #6
+                           [0, 0, 0, 0, 0, 0, 0, 0]] #7
+    board.board[4][2] = 1
+    board.board[4][3] = 1
+    board.board[3][2] = -1
+    board.board[3][3] = -1
+    assert board.board == [[0, 0, 0, 0, 0, 0, 0, 0], #0
+                           [0, 0, 0, 0, 0, 0, 0, 0], #1
+                           [0, 0, 0, 0, 0, 0, 0, 0], #2
+                           [0, 0,-1,-1,-1, 0, 0, 0], #3
+                           [0, 0, 1, 1, 1, 0, 0, 0], #4
+                           [0, 0, 0, 0, 0, 0, 0, 0], #5
+                           [0, 0, 0, 0, 0, 0, 0, 0], #6
+                           [0, 0, 0, 0, 0, 0, 0, 0]] #7
+
+@pytest.fixture
+def board():
+    board1 = Board()
+    board1.board = [[0, 0, 0, 0, 0, 0, 0, 0], #0
+                    [0, 0, 0, 0, 0, 0, 0, 0], #1
+                    [0, 0, 0, 0, 0, 0, 0, 0], #2
+                    [0, 0,-1,-1,-1, 0, 0, 0], #3
+                    [0, 0, 1, 1, 1, 0, 0, 0], #4
+                    [0, 0, 0, 0, 0, 0, 0, 0], #5
+                    [0, 0, 0, 0, 0, 0, 0, 0], #6
+                    [0, 0, 0, 0, 0, 0, 0, 0]] #7
+    board2 = Board()
+    board2.board = [[ 0, 0, 0, 0, 0, 0, 0, 0],#0
+                    [ 0, 0, 0,-1, 0, 0, 0, 0],#1
+                    [-1, 1, 0,-1, 1, 0, 0, 1],#2
+                    [-1,-1,-1,-1, 1,-1,-1, 0],#3
+                    [ 0, 1,-1,-1, 1, 1, 0,-1],#4
+                    [ 0, 0, 0, 1, 0,-1, 1, 0],#5
+                    [ 0, 0, 0, 0, 1, 0, 0, 0],#6
+                    [ 0, 0, 0, 0, 0, 0, 0, 0]]#7
+    return board1, board2
+
+def test_search_color(board):
+    board1, board2 = board
+    assert set(board1.search_color((5, 2), -1)) == {(-1, 0), (-1, 1)}
+    assert set(board1.search_color((5, 3), -1)) == {(-1, 0), (-1, 1), (-1, -1)}
+    assert set(board1.search_color((6, 5), 1)) == set()
+    assert set(board1.search_color((2, 5), 1)) == {(1, -1)}
+    assert set(board1.search_color((4, 1), -1)) == {(0, 1)}
+
+def test_can_set(board):
+    board1, board2 = board
+    assert board1.can_set((5, 2), -1) == True
+    assert board1.can_set((2, 3), 1) == True
+    assert board1.can_set((5, 5), -1) == True
+    assert board1.can_set((2, 4), -1) == False
+    assert board1.can_set((6, 4), -1) == False
+    assert board1.can_set((6, 4), 1) == False
+    assert board1.can_set((4, 1), -1) == False
+
